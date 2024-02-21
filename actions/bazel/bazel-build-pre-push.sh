46f8a02796d30d53d166d49cbca7d97d359e9446
Debkanchan Samadder <debu.samadder@gmail.com>
2024-02-21 20:56:52 +0530

    read from tty

--- actions/bazel/bazel-build-pre-push.sh
+++ actions/bazel/bazel-build-pre-push.sh
@@ -5,32 +5,23 @@ if bazel build //...; then
   echo "Bazel build successful"
   exit 0
 else
-  # while true; do
-  #   read -r trash
-  #   echo $trash
-  #   sleep 1
-  # done
-  echo "Bazel build failed. Do you want to continue? (yes/no): "
-  read -r response </dev/tty
-  echo $response
-  sleep 5
-
-  while ! [[ ${response} =~ ^(yes|y|Yes|no|n|No)$ ]]; do
-    echo "Invalid input. Please enter 'yes' or 'no'."
+  while true; do
+    read -r -p "Bazel build failed. Do you want to continue? (yes/no): " response </dev/tty
+    if [[ ${response} =~ ^(yes|y|Yes)$ ]]; then
+      exit 0
+    elif [[ ${response} =~ ^(no|n|No)$ ]]; then
+      exit 1
+    else
+      echo "Invalid input. Please enter 'yes' or 'no'."
+    fi
   done
-
-  if [[ ${response} =~ ^(yes|y|Yes)$ ]]; then
-    exit 0
-  elif [[ ${response} =~ ^(no|n|No)$ ]]; then
-    exit 1
-  fi
 fi
 
 # Check if MODULE.bazel.lock has been modified
 diff=$(git diff --name-only)
 if echo "${diff}" | grep -q "MODULE.bazel.lock"; then
   echo "MODULE.bazel.lock has been modified"
-  read -r -p "Do you want to commit the file? (yes/no): " response
+  read -r -p "Do you want to commit the file? (yes/no): " response </dev/tty
   if [[ ${response} =~ ^(yes|y|Yes)$ ]]; then
     git add MODULE.bazel.lock
     git commit -m "chore: Update MODULE.bazel.lock"
