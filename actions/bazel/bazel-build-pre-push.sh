1ab7e864955f4394d4af8360d6651db98076b07f
DK <debu.samadder@gmail.com>
2024-02-21 21:04:12 +0530

    feat: bazel build pre push action (#1)
    * feat: add bazel build action
    
    * chore: fix format
    
    * docs: add docs
    
    * add a better shell script
    
    * rename folder
    
    * docs: update docs reference
    
    * fix: lint issue
    
    * rename things
    
    * fix: script path
    
    * chore: simplify script
    
    * refactor
    
    * let's see
    
    * split prompt
    
    * remove prompt flag
    
    * debug
    
    * read trash values
    
    * add sleep
    
    * sleep in seconds
    
    * read response from tty
    
    * read from tty

---
+++ actions/bazel/bazel-build-pre-push.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Run bazel build
+if bazel build //...; then
+  echo "Bazel build successful"
+  exit 0
+else
+  while true; do
+    read -r -p "Bazel build failed. Do you want to continue? (yes/no): " response </dev/tty
+    if [[ ${response} =~ ^(yes|y|Yes)$ ]]; then
+      exit 0
+    elif [[ ${response} =~ ^(no|n|No)$ ]]; then
+      exit 1
+    else
+      echo "Invalid input. Please enter 'yes' or 'no'."
+    fi
+  done
+fi
+
+# Check if MODULE.bazel.lock has been modified
+diff=$(git diff --name-only)
+if echo "${diff}" | grep -q "MODULE.bazel.lock"; then
+  echo "MODULE.bazel.lock has been modified"
+  read -r -p "Do you want to commit the file? (yes/no): " response </dev/tty
+  if [[ ${response} =~ ^(yes|y|Yes)$ ]]; then
+    git add MODULE.bazel.lock
+    git commit -m "chore: Update MODULE.bazel.lock"
+    echo "File committed successfully"
+  else
+    echo "File not committed"
+  fi
+else
+  exit 0
+fi
